a3c75cac177fc40a026c79fa3c3ef3909316fa8e
Lukas Pabst <66950407+lukaspabst@users.noreply.github.com>
2024-01-14 02:35:54 +0100

    Merge Dev into Branch (#4)

--- websiteaboutme/src/components/About/About.js
+++ websiteaboutme/src/components/About/About.js
@@ -55,7 +55,7 @@ const AboutPage = () => {
                                     <span key={wordIndex} style={{
                                         display: 'inline-block',
                                         marginRight: '5px',
-                                        animation: inView ? `appear 2s ease ${wordIndex / 2}s forwards` : ``
+                                        animation: inView ? `appear 2s ease ${wordIndex / 2}s forwards` : `leave 0.25s ease`
                                     }}>{entry.icon}{entry.word}
                                     </span>
                                     {entry.subItems && entry.subItems.map((subItem, subIndex) => (
@@ -63,7 +63,7 @@ const AboutPage = () => {
                                             <span style={{
                                                 display: 'inline-block',
                                                 marginRight: '5px',
-                                                animation: inView ? `appear 2s ease ${subIndex / 2 + wordIndex / 2}s forwards` : ``
+                                                animation: inView ? `appear 2s ease ${subIndex / 2 + wordIndex / 2}s forwards` : `leave 0.25s ease`
                                             }}>
                                                 {subItem.word}{subItem.word && <br/>}
                                             </span>
@@ -75,7 +75,7 @@ const AboutPage = () => {
                                                     <span style={{
                                                         display: 'inline-block',
                                                         marginRight: '5px',
-                                                        animation: inView ? `appear 2s ease ${subSubIndex / 2 + subIndex / 2 + wordIndex / 2}s forwards` : ``
+                                                        animation: inView ? `appear 2s ease ${subSubIndex / 2 + subIndex / 2 + wordIndex / 2}s forwards` : `leave 0.25s ease`
                                                     }}>
                                                         {subSubItem.word}
                                                     </span>
@@ -97,9 +97,9 @@ const AboutPage = () => {
                             <div
                                 style={{
                                     position: 'absolute',
-                                    top: '50%',
+                                    top: '40%',
                                     left: '75%',
-                                    transform: 'translate(-50%, -50%)'
+                                    transform: 'translate(-50%, -40%)'
                                 }}>
                                 <div
                                     className={`image-overlay-toRevealHover ${inView ? 'is-visible' : 'is-hidden'} ${isHovered ? 'is-hidden' : ''}`}>Hover
